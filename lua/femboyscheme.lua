7555ac2c39b5be1e30e7ff7b69c6c4f61f6a048a
3lv <cristian.vlad.vladutu@gmail.com>
2021-05-12 11:04:10 +0300

    FIRST WORKING

--- lua/femboyscheme.lua
+++ lua/femboyscheme.lua
@@ -32,9 +32,8 @@ end
 function f.highlight(group, color)
 	local style = color.style and 'gui='..color.style or 'gui=NONE'
 	local fg = color.fg and 'guifg='..color.fg or 'guifg=NONE'
-	local bg = color.bg and 'guibg='..color.bg or 'guifg=NONE'
+	local bg = color.bg and 'guibg='..color.bg or 'guibg=NONE'
 	local sp = color.sp and 'guisp='..color.sp or ''
-	print( group..' '..style..' '..fg..' '..bg..' '..sp )
 	vim.api.nvim_command('highlight '..group..' '..style..' '..fg..' '..bg..' '..sp)
 end
 
